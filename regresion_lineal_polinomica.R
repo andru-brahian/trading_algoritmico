de0b1947cda8915b626dd26f1580d3461b5cc006
Brahian Andrey Giraldo Alzate <brahian199648@gmail.com>
2023-08-30 16:29:14 +0200

    quinto commit

--- regresion_lineal_polinomica.R
+++ regresion_lineal_polinomica.R
@@ -1,1 +1,6 @@
 # ACTIVIDAD 1
+
+url <- "https://raw.githubusercontent.com/andru-brahian/trading_algoritmico/main/usados_regresion.csv"
+#datos <- read.csv(url)
+datos <- read.csv(url, sep = ";")
+dataset = read.csv("https://raw.githubusercontent.com/andru-brahian/trading_algoritmico/main/usados_regresion.csv", sep = ";")
